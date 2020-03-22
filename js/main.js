d887820028da4f90bf4315472c38bac91f8bd8bc
Zeeshan_Hasmi <syedzeeshan.hasmi@enclouden.com>
2020-03-22 12:00:44 +0530

    required files added

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+//  On click function for mobile site
+
+function pop1(){
+    console.log('pop1');
+}
+function pop2(){
+    console.log('pop1');
+}
+function pop3(){
+    console.log('pop1');
+}
+function pop4(){
+    console.log('pop1');
+}
+function pop5(){
+    console.log('pop1');
+}
